aa955c4a6236e038d776f236cee13c12d3b6424e
Federico1820 <157184998+Federico1820@users.noreply.github.com>
2024-01-19 20:29:52 -0500

    alerta informativa de números

---
+++ app.js
@@ -0,0 +1,15 @@
+//Variables
+let numeroUsuario = prompt("por favor escribe un numero positivo o negativo:");
+
+if (numeroUsuario > 0) {
+    alert("el numero ingresado es positivo");
+    } else if (numeroUsuario < 0) {
+    alert("el numero ingresado es negativo");
+    } else {
+    alert("el numero ingresado es 0");
+    }
+
+
+
+
+
